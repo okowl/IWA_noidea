6683395f85746c387bb09de637ad8eac77f1ded7
Oli <olli.kiseleva@gmail.com>
2019-12-06 15:39:42 +0000

    minor changes, new script added to swap between update and add tab

--- views/jquery/table.js
+++ views/jquery/table.js
@@ -52,3 +52,22 @@ $(document).ready(function ()
 {
 	draw_table();
 });
+/**
+ * This part of the code was borrowed from my last year project
+ * https://github.com/okowl/CA_2Barbers_web/
+ */
+ function opentab(evt, usertype) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(usertype).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
